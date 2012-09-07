d5b99a01493ca395ca18b6ff5d3d91915a83de6c
Jivko Petiov <jivkopetiov@gmail.com>
2012-09-07 23:49:30 +0300

    initial import

---
+++ src/MoviesApp.Cmd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatTmdb.V3;
+
+namespace MoviesApp.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new Tmdb("e7ea08e0ed9aba51ea90d5ffe68fa672");
+            var result = api.GetPopularMovies(1);
+            foreach (var a in result.results)
+            {
+                Console.WriteLine(a.title);
+            }
+        }
+    }
+}
---
+++ src/MoviesApp.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoviesApp.Core")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5b410834-16c6-4953-b6ed-f04c9e9d20a7")]
